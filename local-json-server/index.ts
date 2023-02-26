89baa599ebcd4101f6ea90649cc49b623a5c846e
kunyaoxu <a663321765@gmail.com>
2023-02-27 03:48:25 +0800

    feat: 🎸 add local-json-server

---
+++ local-json-server/index.ts
@@ -0,0 +1,13 @@
+import jsonServer from 'json-server';
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const PORT = 8888;
+
+server.use(middlewares);
+server.use(router);
+server.listen(PORT, () => {
+  console.log(`JSON Server is running at ${PORT}`);
+});
